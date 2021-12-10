50546076d6f455075d3fc6adae37f4f68c560765
vtyric <misha.torno@gmail.com>
2021-12-10 13:11:22 +0500

    добавил свеггер в проект, теперь буду прописывать руты + вести документацию в свеггере

--- server/index.js
+++ server/index.js
@@ -3,12 +3,15 @@ const express = require('express');
 const sequelize = require('./db');
 const cors = require('cors');
 const models = require('./ORM/models_connections');
+const swagger = require('swagger-ui-express');
+const swaggerDoc = require('./swagger.json');
 
 const PORT = process.env.PORT || 5000;
 const app = new express();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+app.use('/swagger', swagger.serve, swagger.setup(swaggerDoc));
 
 const start = async () => {
     try {
