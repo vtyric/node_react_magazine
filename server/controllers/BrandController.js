989dc20ba4c584b2b2627f08dfc64f5c3e1361e3
vtyric <misha.torno@gmail.com>
2022-01-14 15:53:56 +0500

    сделал бренд контроллер, обновил свеггер + руты, тоже самое как с типами

--- server/controllers/BrandController.js
+++ server/controllers/BrandController.js
@@ -1,14 +1,27 @@
+const {Brand} = require("../ORM/modelsConnections");
+const ApiError = require("../error/ApiError");
+
 class BrandController {
     async create(request, response) {
+        const {name} = request.body;
+        const brand = await Brand.create({name});
 
+        return response.json(brand);
     }
 
     async getAll(request, response) {
+        const brands = await Brand.findAll();
 
+        return response.json(brands);
     }
 
-    async delete(request, response) {
+    async delete(request, response, next) {
+        const {id} = request.query;
+        const brand = await Brand.destroy({where: {id}});
 
+        return !brand
+            ? next(ApiError.badRequest(`пользователь с таким id(${id}) не найден`))
+            : response.json('success');
     }
 }
 
--- server/routes/brandRouter.js
+++ server/routes/brandRouter.js
@@ -3,8 +3,8 @@ const brandController = require('../controllers/BrandController');
 const Router = require('express');
 const router = new Router();
 
-router.post('/', brandController.create);
-router.get('/', brandController.getAll);
-router.delete('/:id', brandController.delete);
+router.post('/create', brandController.create);
+router.get('/get', brandController.getAll);
+router.delete('/delete:id', brandController.delete);
 
 module.exports = router;
