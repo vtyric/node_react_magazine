1353b9da0e4912119d41e111ca23513190f1e0b7
vtyric <misha.torno@gmail.com>
2022-01-16 19:56:26 +0500

    добавил чекер ролей. забыл в прошлый раз добавить)))

--- server/routes/deviceRouter.js
+++ server/routes/deviceRouter.js
@@ -1,11 +1,12 @@
 const deviceController = require('../controllers/DeviceController');
 
 const Router = require('express');
+const authMiddleware = require("../middleware/authMiddleware");
 const router = new Router();
 
-router.post('/create', deviceController.create);
+router.post('/create', authMiddleware('ADMIN'), deviceController.create);
 router.get('/get', deviceController.getAll);
 router.get('/get:id', deviceController.getById);
-router.delete('/delete:id', deviceController.delete);
+router.delete('/delete:id', authMiddleware('ADMIN'), deviceController.delete);
 
 module.exports = router;
